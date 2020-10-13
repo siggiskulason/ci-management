0cd2af0ff9c558d09ace414964ecaa5804fbf18f
Siggi Skulason <61322661+siggiskulason@users.noreply.github.com>
2020-10-13 17:08:10 +0100

    Update build-snap.sh

--- shell/build-snap.sh
+++ shell/build-snap.sh
@@ -3,7 +3,17 @@
 # script to build the edgexfoundry snap on ubuntu
 # installs lxd, snapcraft, and then builds the snap 
 
-snap install lxd
-snap install snapcraft
-lxd init --auto
-snapcraft --use-lxd
+# remove lxd
+sudo apt-get remove -qy --purge lxd lxd-client
+sudo snap remove --purge lxd
+# set up lxd group
+sudo groupadd --force --system lxd
+sudo /usr/sbin/usermod -G lxd -a `whoami`
+newgrp - lxd
+#install lxd
+sudo snap install lxd
+sudo lxd init --auto
+# install snapcraft
+sudo snap install --classic snapcraft
+# run snapcraft
+sg lxd "snapcraft --use-lxd"
